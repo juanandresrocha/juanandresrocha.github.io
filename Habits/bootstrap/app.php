87d89c143cff9fbd5ec5ef27d794f2b750387d66
Mike Bas <mikebp_@hotmail.com>
2017-11-21 15:10:34 -0600

    Fix to Slim API

--- Habits/bootstrap/app.php
+++ Habits/bootstrap/app.php
@@ -6,25 +6,54 @@
  * Time: 1:06 PM
  */
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\App();
 
-$app->get('/{habitID}/', function ($request, $response, $args) {
+$app->get('/habits', function (Request $request, Response $response, $args) {
 
     $FBConnection = new \App\FBConnection();
 
-    $reference = $FBConnection->getDatabase()->getReference('habits/' + $args['habitID']);
+    $reference = $FBConnection->getDatabase()->getReference('habits');
 
     $snapshot = $reference->getSnapshot()->getValue();
 
-    return $response->withStatus(200)->write(print_r($snapshot));
+    return $response->withStatus(200)->write(json_encode($snapshot));
 });
 
-$app->post('/', function ($request, $response, $args) {
+$app->get('/habit/[{id}]', function (Request $request, Response $response, $args) {
+
+    $FBConnection = new \App\FBConnection();
+
+    $reference = $FBConnection->getDatabase()->getReference('habits/' . $args['id']);
+
+    $snapshot = $reference->getSnapshot()->getValue();
+
+    return $response->withStatus(200)->write(json_encode($snapshot));
+});
+
+$app->post('/habit/new', function (Request $request, Response $response, $args) {
 
     $data = $request->getParsedBody();
     $habit = new \App\Models\Habit($data['userID'], $data['name'], $data['difficulty']);
 
     return $response->withStatus(201)->write('Habit generated successfully');
+});
+
+$app->put('/habit/update/{id}', function(Request $request, Response $response, $args){
+    // $habit = \App\Models\Habit::find();
+});
+
+$app->delete('/habit/delete/{id}', function(Request $request, Response $response, $args){
+
+    $FBConnection = new \App\FBConnection();
+
+    $reference = $FBConnection->getDatabase()->getReference('habits/' . $args['id']);
+
+    $reference->remove();
+
+    return $response->withStatus(200)->write('Habit deleted successfully');
 });
